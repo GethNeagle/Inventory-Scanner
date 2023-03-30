84a5f6bb017edbcbf2200b65c2498ad507d07f68
Gethin <gethneagle@gmail.com>
2023-03-30 16:15:26 +0100

    .

--- routes/main.js
+++ routes/main.js
@@ -386,7 +386,7 @@ module.exports = function(app) {
     
         // Save the workbook and send it as a response
         workbook.xlsx.writeBuffer().then((buffer) => {
-          res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+          //res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
           res.attachment('inventory.xlsx');
           res.send(buffer);
         });
